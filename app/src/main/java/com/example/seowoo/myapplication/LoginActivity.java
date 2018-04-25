f2b195d309ade7f4abb00881fb1d9ad6d94d13bb
seowoo <alsmwsk@naver.com>
2018-04-26 06:53:41 +0900

    수정완료?

--- app/src/main/java/com/example/seowoo/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/seowoo/myapplication/LoginActivity.java
@@ -1,13 +1,41 @@
 package com.example.seowoo.myapplication;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.File;
 
 public class LoginActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        imageView = (ImageView)findViewById(R.id.imageTitle);
+
+
+
+        //이미지표시
+
+//        Glide.with(getApplicationContext()).load(R.drawable.mypicture).centerCrop().override(125,125).into(imageView);
+//        Glide.with(getApplicationContext()).load(R.drawable.hoseo).centerCrop().override(125,125).into(imageView);
+        Glide.with(getApplicationContext()).load("http://blogfiles5.naver.net/data19/2007/4/9/227/result_2007_3_1_13_51_57_890_2-qlcrhktodaud.jpg").override(125,125).centerCrop().into(imageView);
+
     }
+
+//    public void class LoadImage(final String firstimage){
+//        imageView = (ImageView)findViewById(R.id.imageTitle);
+//        //이미지 표시
+//        Glide.with(this).load("drawable/hoseo.jpg").override(125,125)
+//        .into(imageView);
+//
+//
+//    }
 }
