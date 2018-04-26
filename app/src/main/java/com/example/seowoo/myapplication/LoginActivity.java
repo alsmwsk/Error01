b12e92158f0a2d792e696266bfa87e1ed2b5d862
seowoo <alsmwsk@naver.com>
2018-04-27 02:44:56 +0900

    수정완료?

--- app/src/main/java/com/example/seowoo/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/seowoo/myapplication/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.example.seowoo.myapplication;
 
+import android.content.Intent;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
@@ -12,14 +15,22 @@ import java.io.File;
 public class LoginActivity extends AppCompatActivity {
 
     ImageView imageView;
+    TextView registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        registerButton = (TextView)findViewById(R.id.registerButton);
         imageView = (ImageView)findViewById(R.id.imageTitle);
-
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registintent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registintent);
+            }
+        });
 
 
         //이미지표시
