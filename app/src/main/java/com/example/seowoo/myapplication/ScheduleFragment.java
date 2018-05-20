4aba33b15b802a6b8a04bc2dc30faa06b4c4dcc2
seowoo <alsmwsk@naver.com>
2018-05-20 15:29:53 +0900

    16번 할차례

--- app/src/main/java/com/example/seowoo/myapplication/ScheduleFragment.java
+++ app/src/main/java/com/example/seowoo/myapplication/ScheduleFragment.java
@@ -2,12 +2,23 @@ package com.example.seowoo.myapplication;
 
 import android.content.Context;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 
 
 /**
@@ -90,12 +101,101 @@ public class ScheduleFragment extends Fragment {
             thursday[i] = (TextView)getView().findViewById(getIDth);
             friday[i] = (TextView)getView().findViewById(getIDf);
         }
+
+        new BackgroundTask().execute();
 //        monday[0] = (TextView)getView().findViewById(R.id.tu0);
 //        monday[0] = (TextView)getView().findViewById(R.id.monday0);
 //        monday[0] = (TextView)getView().findViewById(R.id.monday0);
 //        monday[0] = (TextView)getView().findViewById(R.id.monday0);
 //        monday[0] = (TextView)getView().findViewById(R.id.monday0);
 
+    }
+
+    class BackgroundTask extends AsyncTask<Void, Void, String>
+    {
+        String targert;
+
+        @Override
+        protected void onPreExecute() {
+
+            try{
+                targert = "http://alsmwsk3.dothome.co.kr/Android/ScheduleList.php?userID=" + URLEncoder.encode(MainActivity.userID,"UTF-8");
+            }catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        //좀 많이 어려움
+        @Override
+        protected String doInBackground(Void... voids) {
+            try{
+                URL url = new URL(targert);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                //넘어오는 결과값을 저장할수 있도록 하는 메소드..
+                InputStream inputStream = httpURLConnection.getInputStream();
+                //해당 InputStream에 있는 값을 읽을 수 있도록 하기 위한 것
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"utf-8"));
+                String temp;
+                StringBuilder stringBuilder = new StringBuilder();
+
+                //bufferedReader로 읽어온 값을 temp에다 넣은 것이 null이 아니면 temp에다가 값을 직업넣고 개행을해준다.?
+                while ((temp = bufferedReader.readLine()) != null)
+                {
+                    stringBuilder.append(temp + "\n");
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return  stringBuilder.toString().trim();
+
+            }catch (Exception e){
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        //해당 결과를 처리 할 수 있는 메소드..
+        @Override
+        protected void onPostExecute(String result) {
+
+
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                JSONArray jsonArray = jsonObject.getJSONArray("response");
+                int count = 0;
+                String courseProfessor;
+                String courseTime;
+                String courseTitle;
+
+                int courseID;
+                while (count < jsonArray.length())
+                {
+                    JSONObject object = jsonArray.getJSONObject(count);
+                    courseID = object.getInt("courseID");
+                    courseProfessor = object.getString("courseProfessor");
+                    courseTime = object.getString("courseTime");
+                    courseTitle = object.getString("courseTitle");
+
+                    schedule.addSchedule(courseTime, courseTitle, courseProfessor);
+                    //adapter.notifyDataSetChanged();
+
+                    count++;
+
+                }
+
+            }catch (Exception e){
+                e.printStackTrace();;
+            }
+
+            schedule.setting(monday, tuesday, wednesday, thursday, friday, getContext());
+
+        }
     }
 
     @Override
